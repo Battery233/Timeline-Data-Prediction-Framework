253f1248b7832486326a0ff132e3b129b4e2ad89
yigengw <yigengw@andrew.cmu.edu>
2020-04-08 04:10:38 -0400

    Added docs for Framework and ChangeListener

--- framework/src/main/java/edu/cmu/cs/cs214/hw5/framework/core/StatusChangeListener.java
+++ framework/src/main/java/edu/cmu/cs/cs214/hw5/framework/core/StatusChangeListener.java
@@ -1,6 +1,13 @@
 package edu.cmu.cs.cs214.hw5.framework.core;
 
+/**
+ * The interface of the listener for the changes of status.
+ */
 public interface StatusChangeListener {
 
+    /**
+     * This function will be called when a new plugin is registered.
+     * @param plugin   The new plugin which is to be registered.
+     */
     void onPluginRegistered(Plugin plugin);
 }
